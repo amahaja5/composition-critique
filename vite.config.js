7fab49fdca3a225641df521c3ba989a308744028
Avi Mahajan <mahajan.avi@gmail.com>
2026-05-30 11:51:48 -0400

    verovio is big, chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 8500,
+  },
   optimizeDeps: {
     exclude: ['verovio'],
   },
